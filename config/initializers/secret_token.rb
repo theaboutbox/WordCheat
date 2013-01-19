2c394569c6090fb14523c2c4b9ab3d81c5b2e7b1
Cameron Pope <cameron@theaboutbox.com>
2013-01-18 20:35:49 -0700

    set up template and get data

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WordCheat::Application.config.secret_token = 'db656f7dd895ac8810f0fc1b13d58b091565a9086c5f97b3539c08b31aa4ddfa40e7d63eeb14f98ba458c9ed86f95407be54c61a4187a282d23fd7b032a68c72'
